3078de8ce1f840c27e9788413f8555fd53453893
Aryan-Mehta0402 <153349180+Aryan-Mehta0402@users.noreply.github.com>
2026-06-03 14:30:15 +0530

    Add: combinations-of-a-phone-number - submission-1

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        # n - 2
+        maap = [["a", "b", "c"], ["d", "e", "f"], ["g", "h", "i"],
+                ["j", "k", "l"], ["m", "n", "o"], ["p", "q", "r", "s"],
+                ["t", "u", "v"], ["w", "x", "y", "z"]
+            ]
+
+        res = []
+        sol = ""
+
+        if digits == "":
+            return []
+
+        def bt(n, sol):
+            if n == len(digits):
+                res.append(sol)
+                return
+            
+            for ch in maap[int(digits[n])-2]:
+                bt(n+1, sol+ch)
+
+        bt(0, sol)
+        return res
