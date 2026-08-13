fd92680ab4bf3ecda120e60dd274669c11031a60
Aryan-Mehta0402 <153349180+Aryan-Mehta0402@users.noreply.github.com>
2026-08-13 18:00:43 +0530

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # make two hash maps and check if they are the same
+        if len(s) != len(t):
+            return False
+
+        count = [0]*26
+
+        for i in range(len(s)):
+            count[ord(s[i])-ord('a')] += 1
+            count[ord(t[i])-ord('a')] -= 1
+
+        return all(x == 0 for x in count)
